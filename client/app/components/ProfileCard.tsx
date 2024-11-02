81400b0211cccc5560693427dfd4f8985eb8b715
kishore28kumar <kishore28kks@gmail.com>
2024-11-02 12:25:49 +0530

    feat: copy to clipboard

--- client/app/components/ProfileCard.tsx
+++ client/app/components/ProfileCard.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { PrimaryButton } from "./Button";
+import { useEffect, useState } from "react";
 
-export const ProfileCard = () => {
+export const ProfileCard = ({ publicKey }: { publicKey: string }) => {
   const session = useSession();
   const route = useRouter();
 
@@ -22,7 +24,7 @@ export const ProfileCard = () => {
           image={session.data?.user?.image ?? ""}
           name={session.data?.user?.name ?? ""}
         />
-        <Assets />
+        <Assets publicKey={publicKey} />
       </div>
     </div>
   );
@@ -37,6 +39,35 @@ function Greeting({ image, name }: { image: string; name: string }) {
   );
 }
 
-function Assets() {
-  return <div className="text-xl text-slate-400 pt-3">Account Assets</div>;
+function Assets({ publicKey }: { publicKey: string }) {
+  const [copied, setCopied] = useState(false);
+  useEffect(() => {
+    if (copied) {
+      let timeout = setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+      return () =>{
+        clearTimeout(timeout);
+      } 
+    }
+  }, [copied]);
+  return (
+    <div className="text-xl text-slate-400 pt-3">
+      Account Assets
+      <br />
+      <div className="flex justify-between">
+        <div></div>
+        <div>
+          <PrimaryButton
+            onClick={() => {
+              navigator.clipboard.writeText(publicKey);
+              setCopied(true);
+            }}
+          >
+            {copied ? "Copied" : "Your Wallet Address"}
+          </PrimaryButton>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,21 +1,34 @@
 import { getServerSession } from "next-auth";
 import { ProfileCard } from "../components/ProfileCard";
 import db from "@/app/db";
-import { authConfig } from "../lib/auth"
+import { authConfig } from "../lib/auth";
 
-async function getBalance() {
+async function getUserWallet() {
   const session = await getServerSession(authConfig);
-  db.solWallet.findFirst({
+  const userWallet = await db.solWallet.findFirst({
     where: {
-      userID: session?.user?.uid
-    }
+      userID: session?.user?.uid,
+    },
+    select: {
+      publicKey: true,
+    },
   });
+  if (!userWallet) {
+    return {
+      error: "No Solana Wallet Found assocaited to the user",
+    };
+  }
+  return {error: null, userWallet};
 }
 
 export default async function () {
+  const userWallet = await getUserWallet();
+  if (userWallet.error ||!userWallet.userWallet?.publicKey) {
+    return <>No Solana Wallet Found</>
+  }
   return (
     <div>
-      <ProfileCard />
+      <ProfileCard publicKey={userWallet.userWallet?.publicKey} />
     </div>
   );
 }
