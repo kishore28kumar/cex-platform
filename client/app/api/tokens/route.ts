4623e80e91e283b3d51994a73fd104b1193a82f7
kishore28kumar <kishore28kks@gmail.com>
2024-11-07 21:38:10 +0530

    chore: try catch for getAccount func

--- client/app/api/tokens/route.ts
+++ client/app/api/tokens/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import {getAccount, getAssociatedTokenAddress, getMint} from "@solana/spl-token"
+import {
+  getAccount,
+  getAssociatedTokenAddress,
+  getMint,
+} from "@solana/spl-token";
 import { connection, getSupportedTokens } from "@/app/lib/constants";
 import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
@@ -7,28 +11,40 @@ export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const address = searchParams.get("address") as unknown as string;
   const SupportedTokens = await getSupportedTokens();
-  const balances = await Promise.all(SupportedTokens.map(token => getAccountBalance(token,address)))
+  const balances = await Promise.all(
+    SupportedTokens.map((token) => getAccountBalance(token, address))
+  );
   // ata => associated token account
   // pda => program derived address
   return NextResponse.json({
-      tokens: SupportedTokens.map((token, index) => ({
-        ...token,
-        balance: balances[index]
-      }))
-  })
+    tokens: SupportedTokens.map((token, index) => ({
+      ...token,
+      balance: balances[index],
+    })),
+  });
 }
 
-function  getAccountBalance(token: {
-  name: string;
-  mint: string;
-  native: boolean;
-}, address: string){
-  if(token.native){
+async function getAccountBalance(
+  token: {
+    name: string;
+    mint: string;
+    native: boolean;
+  },
+  address: string
+) {
+  if (token.native) {
     let balance = await connection.getBalance(new PublicKey(address));
     return balance / LAMPORTS_PER_SOL;
   }
-  const ata = await getAssociatedTokenAddress(new PublicKey(token.mint), new PublicKey(address))
-  const account = await getAccount(connection,ata);
-  const mint = await getMint(connection, new PublicKey(token.mint));
-  return Number(account.amount) / (10 ** mint.decimals)
+  const ata = await getAssociatedTokenAddress(
+    new PublicKey(token.mint),
+    new PublicKey(address)
+  );
+  try {
+    const account = await getAccount(connection, ata);
+    const mint = await getMint(connection, new PublicKey(token.mint));
+    return Number(account.amount) / 10 ** mint.decimals;
+  } catch (error) {
+    return 0;
+  }
 }
--- client/app/lib/constants.ts
+++ client/app/lib/constants.ts
@@ -1,39 +1,60 @@
 import { Connection } from "@solana/web3.js";
 import axios from "axios";
+import { console } from "inspector";
 
-let LAST_UPDATED : number | null = null;
-let prices: {[key: string]:{
+let LAST_UPDATED: number | null = null;
+let prices: {
+  [key: string]: {
     price: string;
-}}
-const TOKEN_PRICE_REFRESH_INTERVAL = 60*1000; // every 60s
+  };
+};
+const TOKEN_PRICE_REFRESH_INTERVAL = 60 * 1000; // every 60s
 export let SUPPORTED_TOKENS: {
-    name: string;
-    mint: string;
-    native: boolean;
-}[] = [{
+  name: string;
+  mint: string;
+  native: boolean;
+  price: string;
+}[] = [
+  {
     name: "USDC",
     mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
-    native: false
-},{
+    native: false,
+    price: "1"
+  },
+  {
     name: "USDT",
     mint: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
-    native: false
-},{
+    native: false,
+    price: "1"
+
+  },
+  {
     name: "SOL",
     mint: "So11111111111111111111111111111111111111112",
-    native: false
-}]
+    native: false,
+    price: "180",
+  },
+];
 
 export const connection = new Connection("https://api.mainnet-beta.solana.com");
 
 export async function getSupportedTokens() {
-    if(!LAST_UPDATED || new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_INTERVAL){
-        const response = await axios.get("https://api.jup.ag/price/v2?ids=So11111111111111111111111111111111111111112,")
-        const prices = response.data;
-        LAST_UPDATED =  new Date().getTime();
+  if (
+    !LAST_UPDATED ||
+    new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_INTERVAL
+  ) {
+    try {
+      const response = await axios.get(
+        "https://api.jup.ag/price/v2?ids=So11111111111111111111111111111111111111112,"
+      );
+      const prices = response.data;
+      LAST_UPDATED = new Date().getTime();
+    } catch (error) {
+      console.log(error);
     }
-    return SUPPORTED_TOKENS.map(s => ({
-        ...s,
-        price:  prices[s.name]
-    }))
+  }
+  return SUPPORTED_TOKENS.map((s) => ({
+    ...s,
+    price: prices[s.name],
+  }));
 }
