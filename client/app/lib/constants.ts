f0ac738d59ca57be21ce246bc5a1d84fa0606f5f
kishore28kumar <kishore28kks@gmail.com>
2024-11-08 23:47:23 +0530

    chore: remove price api

--- client/app/lib/constants.ts
+++ client/app/lib/constants.ts
@@ -45,7 +45,7 @@ export async function getSupportedTokens() {
   ) {
     try {
       const response = await axios.get(
-        "https://api.jup.ag/price/v2?ids=So11111111111111111111111111111111111111112,"
+        ""
       );
       const prices = response.data;
       LAST_UPDATED = new Date().getTime();
